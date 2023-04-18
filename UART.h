96e7f206fce49776fe8b0e255dc1fe64e5819ed0
Josefina <112643341+Data-Joos@users.noreply.github.com>
2023-04-18 17:08:49 +0200

    Update UART.h

--- UART.h
+++ UART.h
@@ -1,12 +1,12 @@
-ifndef  __UART_H 
-#define  __UART_H
+#ifndef  __UART_H //Checks if whether a macro is not defined
+#define  __UART_H //Creates a macro called __UART_H
 
 
-#include "stm32f4xx.h"                 
-#include <stdio.h>
+#include "stm32f4xx.h" //Fetch and include library "stm32f4xx.h"                
+#include <stdio.h> // Fetch and include library <stdio.h>
 
-void USART2_Init(void);
-void test_setup(void);
+void USART2_Init(void); //Declare void
+void test_setup(void); //Declare void
 
 
-#endif
+#endif // Ends #ifndef
