a9a4606dc4fb870a008a977a477423076b973a89
Josefina <112643341+Data-Joos@users.noreply.github.com>
2023-04-19 12:43:03 +0200

    Create LED.c

---
+++ LED.c
@@ -0,0 +1,7 @@
+//Konstruktior: En funktion som etablerar parametrar för LED-applikation
+void Led_ctor(Led_Type * const me, LedColor_Type _
+
+//En funktion som sätter och kontrollerar status på LED
+
+
+//En funktion som utger status på LED
