b29fafe6af82a00d9a8245343fb72f6f2980bcc8
Josefina <112643341+Data-Joos@users.noreply.github.com>
2023-04-19 13:05:40 +0200

    Update LED.c

--- LED.c
+++ LED.c
@@ -1,7 +1,20 @@
+#include "LED.h"
+
 //Konstruktior: En funktion som etablerar parametrar för LED-applikation
-void Led_ctor(Led_Type * const me, LedColor_Type _
+void Led_ctor(Led_Type * const me, LedColor_Type _color, LedState_Type _state){
+  me->color =_color;
+  me->state = _state;
+}
 
 //En funktion som sätter och kontrollerar status på LED
+void Led_setState(Led_type * const me, LedState_Type _state){
+  me->state = _state;
+  
+  switch(me->color){
+    case RED:
+      printf("Den röda LED-lampan är satt till %d \n\r",me->state);
+      }
+}
 
 
 //En funktion som utger status på LED
