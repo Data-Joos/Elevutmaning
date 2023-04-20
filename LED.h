8442956c50986024a8b86210e2c71fdc48e89ce0
Josefina <112643341+Data-Joos@users.noreply.github.com>
2023-04-20 09:23:05 +0200

    Update LED.h

--- LED.h
+++ LED.h
@@ -33,34 +33,35 @@ ifndef LED_H
 
 #include <stdint.h>
 #include "uart.h"
-#include "stm32f4xx.h"                  // Device header
+#include "stm32f4xx.h"  // Device header
 
-#define	LED_PORT 		GPIOB
+//Definerar vilken GPIO som skall vara ansvarig för LED-funktionen
+#define	LED_PORT GPIOB
+//Definerar klocksignalen för porten
+#define	LED_PORT_CLOCK (1U<<1)
+//Definerar de olika pinsen för de olika LED-färgerna
+#define LED_RED_PIN (1U<<14)
+#define LED_GREEN_PIN (1U<<12)
+#define	LED_BLUE_PIN (1U<<15)
+#define LED_YELLOW_PIN (1U<<13)
 
-#define	LED_PORT_CLOCK  (1U<<1)
-
-#define LED_RED_PIN			(1U<<14)
-#define LED_GREEN_PIN		(1U<<12)
-#define	LED_BLUE_PIN		(1U<<15)
-#define LED_YELLOW_PIN	(1U<<13)
-
+//Definerar mode bits för varje LED-färg
 #define LED_RED_MODE_BIT  (1U<<28)
 #define LED_GREEN_MODE_BIT (1U<<24)
 #define LED_YELLOW_MODE_BIT (1U<<26)
 #define LED_BLUE_MODE_BIT (1U<<30)
 
 
-
+//Definerar de olika LED-färgerna som finns
 typedef  enum {
-  
-	RED		= 0,
-	GREEN,
-	YELLOW,
-	BLUE
+  RED = 0,
+  GREEN,
+  YELLOW,
+  BLUE
 
 }LedColor_Type;
 
-
+//Definerar ju/numrerar de olika lägena som LED kan befinna sig i
 typedef enum{
  OFF =0,
  ON = 1
@@ -68,7 +69,7 @@ typedef enum{
 
 
 /*Led's attributes*/
-
+//Definerar de attribut som LED består utav
 typedef struct{
 	
  LedColor_Type color; 
@@ -76,7 +77,7 @@ typedef struct{
 	
 }Led_Type;
 
-
+//Definerar funktionsprototyperna för led-konstruktorn samt statuskontrollen
 void Led_ctor(Led_Type * const me, LedColor_Type _color, LedState_Type _state);
 void Led_setState(Led_Type * const me,LedState_Type _state);
 LedState_Type Led_getState(Led_Type * const me);
