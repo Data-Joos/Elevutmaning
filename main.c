12f9cea61c59b69e1e40c3b653dc0fbff6737a90
Josefina <112643341+Data-Joos@users.noreply.github.com>
2023-04-20 10:22:24 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include "uart.h" //Fetch and include our header file, with functions and library.
+#include "led.h" 
 
+Led_Type led1;
+Led_Type led2;
 
 int main(){ //Create main function
   
 	USART2_Init(); //Init function
+	Led_ctor(&led1,RED,OFF);
+	Led_ctor(&led2,BLUE,ON);
+	
+	Led_getState(&led1);
+	Led_setState(&led2,OFF);
 	
 	//test_setup();
 	printf("Hello there from main function"); //Print
