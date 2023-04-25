e8a61c9f8f95442903425f7974c0c81b07246ad7
Josefina <112643341+Data-Joos@users.noreply.github.com>
2023-04-25 18:57:04 +0200

    Update UART.h

--- UART.h
+++ UART.h
@@ -1,12 +1,14 @@
+//This file combines UART.c and Main.c
+
 #ifndef  __UART_H //Checks if whether a macro is not defined
-#define  __UART_H //Creates a macro called __UART_H
+#define  __UART_H //Init the start of our header file 
 
 
 #include "stm32f4xx.h" //Fetch and include library "stm32f4xx.h"                
-#include <stdio.h> // Fetch and include library <stdio.h>
+#include <stdio.h> // Includes Standard I/O for C
 
-void USART2_Init(void); //Declare void
-void test_setup(void); //Declare void
+void USART2_Init(void); //Calls and references to the declaration from our UART-function
+void test_setup(void); //Calls and references to our test function from UART.c
 
 
-#endif // Ends #ifndef
+#endif // Ends our header file
