cce783903094e0ff3ad65af48cf29804aeb86090
Josefina <112643341+Data-Joos@users.noreply.github.com>
2023-04-25 20:43:08 +0200

    Update LED.h

--- LED.h
+++ LED.h
@@ -1,98 +1,49 @@
 #ifndef LED_H
-#define LED_H //Definerar att headern betecknas med LED.h och skall inkluderas vid benämning
+#define LED_H //Defines that the header is denoted by LED.h and shall be included when called
 
-#include <stdint.h> //Inkluderar ett av c:s standardbibliotek
-#include "UART.h" //Inkluderar vårt UART-drivrutin
+#include <stdint.h> //Includes a standard library that will give us exact-width integer types regardless of underlying hardware(int8_t, uint8_t, int16_t..)
+#include "UART.h" //Includes our UART.h Header
+#include "stm32f4xx.h" //Device header for device-specific information about the hardware.
 
-typedef enum { //Bygger en funktion som numrerar de olika färgerna som kan antas av LED
-  RED = 0,
-  GREEN,
-  YELLOW,
-  BLUE
-}LedColor_Type;
-
-typedef enum {//Bygger en funktion som numrerar de olika statusarna som
-  OFF = 0,
-  ON = 1,
-}LedState_Type;
-
-typedef struct{
-  LedColor_Type color;
-  LedState_Type state;
-}Led_Type;
-
-void Led_ctor(Led_Type * const me, LedColor_Type _color, LedState_Type _state);
-void Led_setState(Led_Type * const me, LedState_Type _state);
-LedState_Type Led_getState(Led_Type * const me);
-
-#endif
-
-
-ifndef LED_H
-#define LED_H
-
-#include <stdint.h>
-#include "uart.h"
-#include "stm32f4xx.h"  // Device header
-
-//Definerar vilken GPIO som skall vara ansvarig för LED-funktionen
+//Difines which GPIO shall be responsible for the LED function
 #define	LED_PORT GPIOB
-//Definerar klocksignalen för porten
+
+//Defines the clock signal for the port
 #define	LED_PORT_CLOCK (1U<<1)
-//Definerar de olika pinsen för de olika LED-färgerna
+
+//Defines the different pins for the different LED colors
 #define LED_RED_PIN (1U<<14)
 #define LED_GREEN_PIN (1U<<12)
 #define	LED_BLUE_PIN (1U<<15)
 #define LED_YELLOW_PIN (1U<<13)
 
-//Definerar mode bits för varje LED-färg
+//Defines mode bits for each LED color
 #define LED_RED_MODE_BIT  (1U<<28)
 #define LED_GREEN_MODE_BIT (1U<<24)
 #define LED_YELLOW_MODE_BIT (1U<<26)
 #define LED_BLUE_MODE_BIT (1U<<30)
 
-
-//Definerar de olika LED-färgerna som finns
-typedef  enum {
+typedef enum { //Builds a function that enumerates the different colors that can be assumed by the LED
   RED = 0,
   GREEN,
   YELLOW,
   BLUE
-
 }LedColor_Type;
 
-//Definerar ju/numrerar de olika lägena som LED kan befinna sig i
-typedef enum{
- OFF =0,
- ON = 1
+typedef enum { //Builds a function that enumerates the different states the LED can be in
+  OFF = 0,
+  ON = 1,
 }LedState_Type;
 
-
-/*Led's attributes*/
-//Definerar de attribut som LED består utav CLASS ÄNDRA
+//Defines the attributes that the LED consists of
 typedef struct{
-	
- LedColor_Type color; 
- LedState_Type state;  
-	
+  LedColor_Type color;
+  LedState_Type state;
 }Led_Type;
 
-//Definerar funktionsprototyperna för led-konstruktorn samt statuskontrollen
+//Declares the functions for the LED construction and the status check
 void Led_ctor(Led_Type * const me, LedColor_Type _color, LedState_Type _state);
-void Led_setState(Led_Type * const me,LedState_Type _state);
+void Led_setState(Led_Type * const me, LedState_Type _state);
 LedState_Type Led_getState(Led_Type * const me);
-#endif
 
-class Led{
-	
-	private:
-	LedColor_Type color;
-	LedState_type state;
-	
-public:
-	Led(LedColor_Type _color,LedState_type _state);
-	void setState(LedState_Type _state);
-	LedState_Type getState() const;
-	
-};
-	
+#endif //Clarifies the end of our header file
